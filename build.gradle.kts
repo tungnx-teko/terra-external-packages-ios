268825f36413c93a0de29d3091f94eb06b29a8ea
Tan TV <tan.tv@teko.vn>
2020-10-07 10:22:33 +0700

    Initial project

---
+++ build.gradle.kts
@@ -0,0 +1,172 @@
+@file:Suppress("UNCHECKED_CAST")
+
+import groovy.json.JsonSlurper
+import groovy.util.Node
+import org.gradle.api.publish.maven.MavenPom
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    id("maven-publish")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "TerraExternalPackages"
+            url = uri("https://maven.pkg.github.com/teko-vn/terra-external-packages")
+            credentials {
+                username = System.getenv("GITHUB_USER_NAME")
+                password = System.getenv("GITHUB_USER_TOKEN")
+            }
+        }
+    }
+    publications.create<MavenPublication>("release") {
+        afterEvaluate {
+            File("build/outputs/aar")
+                .listFiles { pathname -> pathname.isFile && pathname.exists() && pathname.extension == "json" }
+                .orEmpty()
+                .forEach { properties ->
+                    val json = JsonSlurper().parseText(properties.readText())
+                    val artifactProperties = parseArtifactProperties(json as Map<String, Any>)
+                    if (artifactProperties.isValid()) {
+                        groupId = "vn.teko.android.packages"
+                        artifactId = artifactProperties.id
+                        version = artifactProperties.version
+                        artifact("build/outputs/aar/${artifactProperties.name}")
+
+                        pom {
+                            fillGenericDetails()
+                            fillDependencies(artifactProperties.dependencies)
+                        }
+                    }
+                }
+        }
+    }
+}
+
+fun MavenPom.fillGenericDetails() {
+    name.set("Terra external packages")
+    packaging = "aar"
+    description.set("External packages for Terra platform")
+    url.set("https://github.com/teko-vn/terra-external-packages")
+
+    scm {
+        url.set("https://github.com/teko-vn/terra-external-packages")
+        connection.set("scm:git@github.com:teko-vn/terra-external-packages.git")
+        developerConnection.set("scm:git@github.com:teko-vn/terra-external-packages.git")
+    }
+
+    developers {
+        developer {
+            id.set("teko-vn")
+            name.set("Mobile Lab")
+            email.set("mobile.lab@teko.vn")
+            organization.set("Teko")
+            organizationUrl.set("https://teko.vn/")
+        }
+    }
+}
+
+fun MavenPom.fillDependencies(dependencies: List<Dependency>) {
+    withXml {
+        if (dependencies.isNotEmpty()) asNode().appendDependencies(dependencies)
+    }
+}
+
+fun Node.appendDependencies(
+    dependencies: List<Dependency>
+) = appendNode("dependencies").apply {
+    dependencies.forEach { appendDependency(it) }
+}
+
+fun Node.appendDependency(
+    dependency: Dependency
+) = appendNode("dependency").apply {
+    appendNode("groupId", dependency.groupId)
+    appendNode("artifactId", dependency.artifactId)
+    appendNode("version", dependency.version)
+    appendNode("scope", dependency.scope)
+
+    if (dependency.exclusions.isNotEmpty()) appendExclusions(dependency.exclusions)
+}
+
+fun Node.appendExclusions(
+    exclusions: List<Exclusion>
+) = appendNode("exclusions").apply {
+    exclusions.forEach { appendExclusion(it) }
+}
+
+fun Node.appendExclusion(
+    exclusion: Exclusion
+) = appendNode("exclusion").apply {
+    appendNode("groupId", exclusion.groupId)
+    appendNode("artifactId", exclusion.artifactId)
+}
+
+fun parseArtifactProperties(json: Map<String, Any>): ArtifactProperties {
+    val dependencies = mutableListOf<Dependency>()
+    (json["dependencies"] as? List<Map<String, Any>>)?.forEach {
+        val exclusions = mutableListOf<Exclusion>()
+        (it["exclusions"] as? List<Map<String, Any>>)?.forEach {
+            val exclusion = Exclusion(
+                it.getAsString("groupId"),
+                it.getAsString("artifactId")
+            )
+            if (exclusion.isValid()) exclusions.add(exclusion)
+        }
+
+        val dependency = Dependency(
+            it.getAsString("scope"),
+            it.getAsString("groupId"),
+            it.getAsString("artifactId"),
+            it.getAsString("version"),
+            exclusions
+        )
+        if (dependency.isValid()) dependencies.add(dependency)
+    }
+
+    return ArtifactProperties(
+        json.getAsString("id"),
+        json.getAsString("name"),
+        json.getAsString("version"),
+        dependencies
+    )
+}
+
+fun Map<String, Any>.getAsString(key: String, default: String = ""): String {
+    return this[key] as? String ?: default
+}
+
+data class ArtifactProperties(
+    val id: String,
+    val name: String,
+    val version: String,
+    val dependencies: List<Dependency>
+)
+
+fun ArtifactProperties.isValid() = id.isNotBlank() && name.isNotBlank() && version.isNotBlank()
+
+data class Dependency(
+    val scope: String,
+    val groupId: String,
+    val artifactId: String,
+    val version: String,
+    val exclusions: List<Exclusion>
+)
+
+fun Dependency.isValid() = groupId.isNotBlank() && artifactId.isNotBlank() && version.isNotBlank() && scope.isNotBlank()
+
+data class Exclusion(
+    val groupId: String,
+    val artifactId: String
+)
+
+fun Exclusion.isValid() = groupId.isNotBlank() && artifactId.isNotBlank()
---
+++ buildSystem/scripts/download_artifact.py
@@ -0,0 +1,96 @@
+import base64
+import json
+import logging
+import os
+import pathlib
+import requests
+
+
+def authenticate_iam(base_url, client_id, scope, user_name, user_password):
+    url = f"{base_url}/oauth/token"
+    headers = {
+        "content-type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "client_id": client_id,
+        "grant_type": "password",
+        "username": user_name,
+        "password": user_password,
+        "scope": scope
+    }
+    response = requests.post(url=url, headers=headers, data=data)
+
+    if response.ok:
+        content = json.loads(response.content)
+        token_type = content["token_type"]
+        access_token = content["access_token"]
+        return token_type + " " + access_token
+    else:
+        response.raise_for_status()
+        return ""
+
+
+def fetch_artifact_info(base_url, miniapp_id, miniapp_version_id, miniapp_asset_ids, iam_token):
+    url = f"{base_url}/api/v1/mini-apps/{miniapp_id}/versions/{miniapp_version_id}/manifests/download"
+    headers = {
+        "Authorization": iam_token
+    }
+    params = {
+        "assetIds": miniapp_asset_ids
+    }
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.ok:
+        content = json.loads(response.content)
+        assets = content["data"]
+        for asset in assets:
+            download_asset(asset["name"], asset["content"])
+    else:
+        response.raise_for_status()
+
+
+def download_asset(name, content_url):
+    response = requests.get(url=content_url)
+    if response.ok:
+        pathlib.Path('build/outputs/aar').mkdir(parents=True, exist_ok=True)
+
+        file = open(f"build/outputs/aar/{name}", "wb+")
+        file.write(base64.b64decode(response.content))
+        file.close()
+    else:
+        response.raise_for_status()
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    iam_base_url = os.environ["IAM_BASE_URL"]
+    iam_client_id = os.environ["IAM_CLIENT_ID"]
+    iam_scope = os.environ["IAM_SCOPE"]
+    iam_user_email = os.environ["IAM_USER_EMAIL"]
+    iam_user_password = os.environ["IAM_USER_PASSWORD"]
+
+    token = authenticate_iam(
+        iam_base_url,
+        iam_client_id,
+        iam_scope,
+        iam_user_email,
+        iam_user_password
+    )
+
+    terra_base_url = os.environ["TERRA_BASE_URL"]
+    miniapp_id = os.environ["MINIAPP_ID"]
+    miniapp_version_id = os.environ["MINIAPP_VERSION_ID"]
+    miniapp_asset_ids = os.environ["MINIAPP_ASSET_IDS"]
+
+    fetch_artifact_info(
+        terra_base_url,
+        miniapp_id,
+        miniapp_version_id,
+        miniapp_asset_ids,
+        token
+    )
+
+
+if __name__ == "__main__":
+    main()
